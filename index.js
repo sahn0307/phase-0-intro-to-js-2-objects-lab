a1f5ca6e11ed61b424382b8ea3f74ca6fb290269
Sung-Jin Ahn <sahn0307@gmail.com>
2024-01-16 11:16:04 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+let employee = {
+    name: "John",
+    streetAddress: "123 Sesame Street"
+  };
+  
+  // 1) Update employee Object with a new key-value pair (non-destructive)
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  // 2) Destructively update employee Object with a new key-value pair (mutating)
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // 3) Delete a key-value pair from employee Object (non-destructive)
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // 4) Destructively delete a key-value pair from employee Object (mutating)
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
